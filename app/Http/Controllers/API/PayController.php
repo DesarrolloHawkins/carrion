6ee5d8344a06b732757ac81882248463ea14e93b
jgonmor <antonio@hawkins.es>
2024-09-16 22:24:10 +0200

    añadir reservas al paycontroller

--- app/Http/Controllers/API/PayController.php
+++ app/Http/Controllers/API/PayController.php
@@ -101,6 +101,16 @@ class PayController extends Controller
                     'transactionId' => $response->transactionId,
                     'schemeReferenceData' => $response->schemeId,
                 ]);
+
+
+                // Actualizar el estado de la reserva
+                $reserva = Reservas::where('order', $orderId)->first();
+                $reserva->estado = 'pagada';
+                $reserva->transaction = $response->transactionId;
+                $reserva->metodo_pago = 'tarjeta';
+                $reserva->save();
+
+
             } else {
                 return response()->json([
                     'message' => 'Error en el pago: ' . $response->responseMessage,
