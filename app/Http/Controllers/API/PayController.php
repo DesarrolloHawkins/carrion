510fa20a8254e10534f27f9c2c3ceadb85aa343a
jgonmor <antonio@hawkins.es>
2024-09-17 13:29:03 +0200

    Update PayController.php

--- app/Http/Controllers/API/PayController.php
+++ app/Http/Controllers/API/PayController.php
@@ -74,7 +74,7 @@ class PayController extends Controller
             $orderId = $request->input('orderId');
 
             try{
-                $threeDSecureData = Secure3dService::checkEnrollment($card)->execute('default', Secure3dVersion::TWO);
+                $threeDSecureData = Secure3dService::checkEnrollment($card)->execute('default', Secure3dVersion::Any);
                 return $threeDSecureData;
 
             }catch(ApiException $e){
@@ -85,7 +85,113 @@ class PayController extends Controller
                 ]);
             }
           
+            $enrolled = $threeDSecureData->enrolled; // TRUE
+            // if enrolled, the available response data
+            $serverTransactionId = $threeDSecureData->serverTransactionId; // af65c369-59b9-4f8d-b2f6-7d7d5f5c69d5
+            $dsStartProtocolVersion = $threeDSecureData->directoryServerStartVersion; // 2.1.0
+            $dsEndProtocolVersion = $threeDSecureData->directoryServerEndVersion; // 2.1.0
+            $acsStartProtocolVersion = $threeDSecureData->acsStartVersion; // 2.1.0
+            $acsEndProtocolVersion = $threeDSecureData->acsEndVersion; // 2.1.0
+            $methodUrl = $threeDSecureData->issuerAcsUrl; // https://www.acsurl.com/method
+            $encodedMethodData = $threeDSecureData->payerAuthenticationRequest; // Base64 encoded string
 
+            //Prueba al POSTMAN lo que recibe
+            //SIGUIENTE PASO
+            dd("hasta aqui");
+
+            //recibimos la confirmacion y mandamos el mandatory
+                // Add captured browser data from the client-side and server-side 
+            $browserData = new BrowserData();
+            $browserData->acceptHeader = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+            $browserData->colorDepth = ColorDepth::TWENTY_FOUR_BITS;
+            $browserData->ipAddress = "123.123.123.123";
+            $browserData->javaEnabled = TRUE;
+            $browserData->language = "en";
+            $browserData->screenHeight = "1080";
+            $browserData->screenWidth = "1920";
+            $browserData->challengWindowSize = ChallengeWindowSize::FULL_SCREEN;
+            $browserData->timeZone = "0";
+            $browserData->userAgent = "Mozilla/5.0 (Windows NT 6.1; Win64, x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36";
+
+            $threeDSecureData = new ThreeDSecure();
+            $threeDSecureData->serverTransactionId =  $serverTransactionId;
+
+            try {
+                $threeDSecureData = Secure3dService::initiateAuthentication($card, $threeDSecureData)
+                   ->withAmount(0.01)
+                   ->withCurrency("EUR")
+                   ->withOrderCreateDate(date("Y-m-d H:i:s"))
+                   ->withCustomerEmail("james.mason@example.com")
+                   ->withBrowserData($browserData)
+                   ->withMethodUrlCompletion(MethodUrlCompletion::YES)
+                   ->execute(Secure3dVersion::TWO);
+             } catch (ApiException $e) {
+                // TODO: add your error handling here
+             }
+             
+             $status = $threeDSecureData->status;
+
+
+             //SIGUIENTE PASO
+
+             try {
+                $threeDSecureData = Secure3dService::getAuthenticationData()
+                   ->withServerTransactionId($serverTransactionId)
+                   ->execute(Secure3dVersion::TWO);
+             } catch (ApiException $e) { 
+                // TODO: add your error handling here 
+             }
+
+             if (isset($threeDSecureData)) {
+                $status = $threeDSecureData->status; // for example AUTHENTICATION_SUCCESSFUL or AUTHENTICATION_FAILED  
+                // Data required for authorization or database record
+                $authenticationValue = $threeDSecureData->authenticationValue; // ODQzNjgwNjU0ZjM3N2JmYTg0NTM=s
+                $dsTransId = $threeDSecureData->directoryServerTransactionId; // c272b04f-6e7b-43a2-bb78-90f4fb94aa25
+                $messageVersion = $threeDSecureData->messageVersion; // 2.1.0
+                $eci = $threeDSecureData->eci; // 05
+                // Additional response data
+                $acsTransID = $threeDSecureData->acsTransactionId; // 13c701a3-5a88-4c45-89e9-ef65e50a8bf9
+                $statusReason - $threeDSecureData->statusReason; // LOW_CONFIDENCE
+                $authenticationSource = $threeDSecureData->authenticationSource; // BROWSER
+                $messageCategory = $threeDSecureData->messageCategory; // PAYMENT_AUTHENTICATION
+             }
+
+
+             //SIGUIENTE PASO
+
+             // add obtained 3D Secure 2 authentication data
+                $threeDSecureData = new ThreeDSecure();
+                $threeDSecureData->authenticationValue = $authenticationValue;
+                $threeDSecureData->directoryServerTransactionId = $dsTransId;
+                $threeDSecureData->eci = $eci;
+                $threeDSecureData->messageVersion = $messageVersion;
+
+                $card->threeDSecure = $threeDSecureData;
+
+
+                try {
+                    // process an auto-settle authorization
+                    $response = $card->charge(0.01)
+                        ->withCurrency("EUR")
+                        ->execute();
+                } catch (ApiException $e) {
+                    // TODO: Add your error handling here
+                }
+
+
+                if (isset($response)) {
+                    $result = $response->responseCode; // 00 == Success
+                    $message = $response->responseMessage; // [ test system ] AUTHORISED
+                 
+                    // get the details to save to the DB for future requests
+                    $orderId = $response->orderId; // N6qsk4kYRZihmPrTXWYS6g
+                    $authCode = $response->authorizationCode; // 12345
+                    $paymentsReference = $response->transactionId; // 14610544313177922
+                    $schemeReferenceData = $response->schemeId; // MMC0F00YE4000000715
+                 }
+
+
+             //----------------------------------------------------------------------------//
 
 
 
