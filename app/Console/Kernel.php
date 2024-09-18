7112377f3df434b355d732ada2ff3ba4b17d47cf
jgonmor <antonio@hawkins.es>
2024-09-18 23:47:33 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('reservas:cancel-expired')->everyFiveMinutes();
+        $schedule->command('reservas:cancel-expired')->everyMinute();
     }
 
 
